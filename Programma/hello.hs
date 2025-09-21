126876d77026297846e4fe6b3ee7bc1bc4ec37d2
MikTheMilkMan <82846573+MikTheMilkMan@users.noreply.github.com>
2025-09-21 15:25:19 +0200

    geleerd hoe guards werken
    jammer genoeg niet nestbaar, dus misschien toch terugkomen op een if else

--- Programma/hello.hs
+++ Programma/hello.hs
@@ -2,14 +2,16 @@ import Data.Text.Internal.Builder.Functions (i2d)
 main :: IO ()
 main = do 
   
-  print(add 1 2)
-  print(guh "test")
+  print(test 0)
+  print(test 3)
+  print(test 9)
+  print(test 10)
+  print(test 20)
+  -- print(compression testString 1 0)
 
 
-
-
--- testString::String
--- testString = "AAAAABBBBBCCCDDDDEEEEEEF"
+testString::String
+testString = "AAAAABBBBBCCCDDDDEEEEEEF"
 
 
 add :: Integer -> Integer -> Integer
@@ -19,24 +21,43 @@ guh :: String -> Char
 guh input = input!!0
 
 
+test :: Integer -> Integer
+test t 
+  | t == 0 
+  = 20
+  | t > 0
+  = t * 20
+
+
+
+-- compression :: String Integer Integer -> String
+-- compression input index count
+--   | index == input.length
+--   = input index count | input[index] == input[index - 1]
+--   = 
+
+
+
 -- compression :: String Integer Integer -> String
 -- compression input index count = 
---   if index == input.length
---     if(input[index])
+--   if index == input.length then
+--     if input[index] != input[index - 1] then
+--       return input[index - 1] + count + input[index] + 1
+--     else 
+--       return input[index] + (count + 1)
+
+--   else
+--     if input[index] != input[index - 1] then
+--       return input[index - 1] + count + (compression input index + 1 1)
+--     else
+--       return (compression input index + 1 count + 1)
 
 
 
 
 
 
-
-
-
-
-
-
-
-
+  
 -- recursie gok ik denk ik lol
 
 -- String bestaat uit 5x A, 5x B, 3x C, 4x D en 6x E. 
@@ -57,9 +78,4 @@ guh input = input!!0
 -- 5. Doe dit ook voor een grotere en ingewikkeldere afbeelding. 
 -- 6. Mocht dit binnen de gegeven tijd zijn gelukt, zou je kunnen kijken of de compressie ook werkt voor een gif.
 -- Victorie^2 !!! Het werkt nog beter !!!
-
-
-
-
-
 
