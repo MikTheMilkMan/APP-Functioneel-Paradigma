5c17ccd46b3e6249ff4789545194c46c54d876e3
MikTheMilkMan <82846573+MikTheMilkMan@users.noreply.github.com>
2025-09-28 17:19:52 +0200

    code opgeschoond
    ik bedenk me nu nog allerlei cases waar ik nog geen rekening mee heb gehouden maar ik denk dat ik daar niks mee ga doen (het comprimeren van getallenreeksen is momenteel niet mogelijk, evenals het volledig correct decomprimeren van strings met spaties (misschien eerst converteren naar ander type karakter? geen idee, dat wordt onderzoek doen))

--- app/Main.hs
+++ app/Main.hs
@@ -1,67 +1,41 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Use guards" #-}
-
-import Data.Char ( intToDigit, isDigit, digitToInt )
 import Data.List.Split ( split, startsWithOneOf, condense, oneOf )
 import Data.Integer.Conversion ( stringToInteger )
 
 main :: IO ()
 main = do
-    -- print("guh")
-    -- print(characterGiver 'A' 4)
-    -- print(pairGiver "A22B23D46G88")
-    -- print(charAndAmountSplitter ((pairGiver "A22B23D46G88")!!0))
-    -- print(charAndAmountSplitter "A22B23D46G88")
+      putStrLn "This is a demonstration of the text compression algorithm:"
+      putStrLn " "
 
-    -- print(pairGiver "A12B23C4D8e1f2g13")
-    -- print(length (pairGiver "A12B23C4D8e1f2g13"))
-    -- print(charAndAmountSplitter ((pairGiver "A12B23C4D8e1f2g13")!!0) )
-    -- print(charAndAmountSplitter ((pairGiver "A12B23C4D8e1f2g13")!!1) )
-    -- print(charAndAmountSplitter ((pairGiver "A12B23C4D8e1f2g13")!!2) )
-    -- print(charAndAmountSplitter ((pairGiver "A12B23C4D8e1f2g13")!!3) )
-    -- print(charAndAmountSplitter ((pairGiver "A12B23C4D8e1f2g13")!!4) )
-    -- print(charAndAmountSplitter ((pairGiver "A12B23C4D8e1f2g13")!!5) )
-    -- print(charAndAmountSplitter ((pairGiver "A12B23C4D8e1f2g13")!!6) )
-
-    -- print((charAndAmountSplitter ((pairGiver "A12B23C4D8e1f2g13")!!0))!!0)
-    -- print((charAndAmountSplitter ((pairGiver "A12B23C4D8e1f2g13")!!1))!!0)
-    -- print((charAndAmountSplitter ((pairGiver "A12B23C4D8e1f2g13")!!2))!!0)
-    -- print((charAndAmountSplitter ((pairGiver "A12B23C4D8e1f2g13")!!3))!!0)
-    -- print((charAndAmountSplitter ((pairGiver "A12B23C4D8e1f2g13")!!4))!!0)
-    -- print((charAndAmountSplitter ((pairGiver "A12B23C4D8e1f2g13")!!5))!!0)
-    -- print((charAndAmountSplitter ((pairGiver "A12B23C4D8e1f2g13")!!6))!!0)
-
-    -- print((charAndAmountSplitter ((pairGiver "A12B23C4D8e1f2g13")!!0))!!1)
-    -- print((charAndAmountSplitter ((pairGiver "A12B23C4D8e1f2g13")!!1))!!1)
-    -- print((charAndAmountSplitter ((pairGiver "A12B23C4D8e1f2g13")!!2))!!1)
-    -- print((charAndAmountSplitter ((pairGiver "A12B23C4D8e1f2g13")!!3))!!1)
-    -- print((charAndAmountSplitter ((pairGiver "A12B23C4D8e1f2g13")!!4))!!1)
-    -- print((charAndAmountSplitter ((pairGiver "A12B23C4D8e1f2g13")!!5))!!1)
-    -- print((charAndAmountSplitter ((pairGiver "A12B23C4D8e1f2g13")!!6))!!1)
-
-
-    -- print(stringToInteger ((charAndAmountSplitter ((pairGiver "A12B23C4D8e1f2g13")!!1))!!1))
-
-
-    -- print (decompression (pairGiver "A12B23C4D8e1f2g13") 0)
-    -- print (decompression ["A5"] 0)
-    -- print (length "AAAAAAAAAAAAAAAAA")
-    -- print(pairGiver "A5B5C3D4E6F1G1H1I1J1j2J1")
-    -- print (decompression (pairGiver "A5B5C3D4E6F1G1H1I1J1j2J1") 0)
-
-      print (compression testString 1 1)
-      print (decompression (compression testString1 1 1))
-      print ((decompression (compression testString1 1 1)) == testString1)
-
-      print ("========================")
+      print ("=== COMPRESSION ===")
+      print (compressionOneLine testString1 1 1)
+      print (decompressionOneLine (compressionOneLine testString1 1 1))
+      print ((decompressionOneLine (compressionOneLine testString1 1 1)) == testString1)
       
-      print (compression testString2 1 1) 
-      print (decompression (compression testString2 1 1))
-      print ((decompression (compression testString2 1 1)) == testString2)
+      putStrLn " "
+      print ("=== DECOMPRESSION ===")
+      print (compressionOneLine testString2 1 1) 
+      print (decompressionOneLine (compressionOneLine testString2 1 1))
+      print ((decompressionOneLine (compressionOneLine testString2 1 1)) == testString2)
 
-    
+      putStrLn " "
 
+      print ("=== COMPRESSION - MULTIPLE LINES ===")
+      print (compressionMultipleLines testStrings1 0)
 
+      putStrLn " "
+      print ("=== DECOMPRESSION - MULTIPLE LINES ===")
+      print (decompressionMultipleLines testStrings2 0)
+
+      putStrLn "\n"
+      putStrLn "Please type a String you would like to compress:"
+      userInput <- getLine
+      putStrLn "Thank you for your input. This is the result:"
+      print(userInput)
+      print(compressionOneLine userInput 1 1)
+      print(decompressionOneLine (compressionOneLine userInput 1 1))
+      
+
+-- VARIABELEN OM MEE TE TESTEN
 
 testString1::String
 testString1 = "AAAAABBBBBCCCDDDDEEEEEEFGHIJjjJ"
@@ -69,56 +43,75 @@ testString1 = "AAAAABBBBBCCCDDDDEEEEEEFGHIJjjJ"
 testString2::String
 testString2 = "AAAAAAAAAABBBBBCCCDDDDEEEEEEFGHIJjjJ"
 
+testStrings1::[String]
+testStrings1 = ["test", "aaaa", "guhhhhh"]
+
+testStrings2::[String]
+testStrings2 = ["t1e1s1t1", "a4", "g1u1h5"]
 
 
 
-compression :: String -> Int -> Int -> String
-compression input index count =
-  if index == length input - 1 then
+-- CODE
+
+compressionOneLine :: String -> Int -> Int -> String
+compressionOneLine input index count 
+  | index == length input - 1 =
     if input!!index /= input!!(index - 1) then
-      [input!!(index - 1)] ++ show count ++ [input!!index] ++ show 1
+      [input!!(index - 1)] ++ show count ++ [input!!index, '1']
     else
       [input!!index] ++ show (count + 1)
 
 
-  else
+  | otherwise =
     if input!!index /= input!!(index - 1) then
-      [input!!(index - 1)] ++ show count ++ compression input (index + 1) 1
+      [input!!(index - 1)] ++ show count ++ compressionOneLine input (index + 1) 1
     else
-      compression input (index + 1) (count + 1)
+      compressionOneLine input (index + 1) (count + 1)
+
+compressionMultipleLines :: [String] -> Int -> [String]
+compressionMultipleLines input index
+  | index == length input - 1 =
+    [compressionOneLine (input!!index) 1 1]
+  | otherwise = 
+    [compressionOneLine (input!!index) 1 1] ++ compressionMultipleLines input (index+1)
 
 
+decompressionMultipleLines :: [String] -> Int -> [String]
+decompressionMultipleLines input index
+  | index == length input - 1 =
+    [decompressionOneLine (input!!index)]
+  | otherwise =
+    [decompressionOneLine (input!!index)] ++ decompressionMultipleLines input (index+1)
 
-  
-
-decompression :: String -> String
-decompression input =
+decompressionOneLine :: String -> String
+decompressionOneLine input =
   decode (split (startsWithOneOf ['A'..]) input) 0
 
 
 decode :: [String] -> Int -> String
-decode input index =    
-    if index == length input - 1  {- 0 -} then 
-      characterGiver 
-        ((charAndAmountSplitter (input!!index))!!0!!0) 
-        (stringToInteger ((charAndAmountSplitter (input!!index))!!1)) 
-    else 
-      characterGiver 
-        ((charAndAmountSplitter (input!!index))!!0!!0) 
-        (stringToInteger ((charAndAmountSplitter (input!!index))!!1))
-      ++ decode input (index+1)
+decode input index 
+  | index == length input - 1 = 
+    characterGiver 
+      ((charAndAmountSplitter (input!!index))!!0!!0) 
+      (stringToInteger ((charAndAmountSplitter (input!!index))!!1)) 
+  | otherwise = 
+    characterGiver 
+      ((charAndAmountSplitter (input!!index))!!0!!0) 
+      (stringToInteger ((charAndAmountSplitter (input!!index))!!1))
+    ++ decode input (index+1)
+
 
 characterGiver :: Char -> Integer -> String
-characterGiver char count =
-  if count > 1 then
+characterGiver char count 
+  | count > 1 =
     char : characterGiver char (count - 1)
-  else
+  | otherwise =
     [char]
+
 
 charAndAmountSplitter :: String -> [String]
 charAndAmountSplitter input = 
     split(condense(oneOf['0'..'9'])) input
-
 
 
 
@@ -136,29 +129,4 @@ add x y = x + y
 
 guh :: String -> Char
 guh input = input!!1
-
-
-
-
-
--- recursie gok ik denk ik lol
-
--- String bestaat uit 5x A, 5x B, 3x C, 4x D en 6x E. 
--- Als deze String door een RLE-compressie-algoritme wordt gehaald, moet het resultaat
--- dus "A5B5C3D4E6" zijn
-
-
-
-
--- Wat wil ik bereiken hier?
--- RLE-compressie
--- Hoe ga ik dat doen?
--- 1. Begin met het comprimeren van een String
--- 2. Ga daarna aan de slag met het decomprimeren van die String
--- 3. Als dat werkt, begin met het uitlezen van de data van een kleine en simpele zwart-wit afbeelding,
--- 4. Comprimeer de uitgelezen data en print deze
--- Victorie !! Het werkt !!
--- 5. Doe dit ook voor een grotere en ingewikkeldere afbeelding. 
--- 6. Mocht dit binnen de gegeven tijd zijn gelukt, zou je kunnen kijken of de compressie ook werkt voor een gif.
--- Victorie^2 !!! Het werkt nog beter !!!
 
